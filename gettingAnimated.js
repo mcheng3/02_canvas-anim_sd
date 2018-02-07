86a0a51dec64a849373200a372aba2a31ccadfae
mcheng3 <michael.cheng65@gmail.com>
2018-02-07 11:37:15 -0500

    Update js

--- gettingAnimated.js
+++ gettingAnimated.js
@@ -5,7 +5,7 @@ var animate = function(){
     var ctx = c.getContext("2d");
     ctx.fillStyle = "#99348d";
     var pos = 0;
-    var r = 250;
+    var m = 250;
 
     var requestID;
     var cancel = function(){
@@ -14,7 +14,8 @@ var animate = function(){
     var drawCircle = function(){
 	ctx.clearRect(0, 0, 500, 500);
 	ctx.beginPath();
-	ctx.arc(250,250,r*(0.5*Math.sin(r)+0.5), 0, 2*Math.PI);
+	ctx.arc(250,250,250*(0.5*Math.sin(m/50)+0.5), 0, 2*Math.PI);
+	m++;
 	ctx.stroke();
 	ctx.fill();
 	pos+=0.5;
